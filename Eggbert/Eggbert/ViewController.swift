351d37ef08b5feeeae2467e52701a90678491eca
Andydenike <denikeandrew@gmail.com>
2019-08-29 14:35:29 -0700

    Added first project components

---
+++ Eggbert/Eggbert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eggbert
+//
+//  Created by Grandma Denny on 8/28/19.
+//  Copyright © 2019 GrandmaDennyInnovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

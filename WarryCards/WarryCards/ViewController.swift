933ce20277ca2e5cb89bfcc4fb183a13885cbef0
Andydenike <denikeandrew@gmail.com>
2019-09-02 16:43:08 -0700

    Added project

---
+++ WarryCards/WarryCards/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WarryCards
+//
+//  Created by Grandma Denny on 8/29/19.
+//  Copyright © 2019 GrandmaDennyInnovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c8509ad22867833c65119527c250261c04ee757c
Andydenike <denikeandrew@gmail.com>
2019-09-01 18:50:10 -0700

    Added project / added content

---
+++ RealLifeWithFriends/RealLifeWithFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealLifeWithFriends
+//
+//  Created by Grandma Denny on 8/30/19.
+//  Copyright © 2019 GrandmaDennyInnovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
